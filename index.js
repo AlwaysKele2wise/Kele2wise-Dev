7369e18d2ffccbcb5832cf3fa86c5506aadd4a43
AlwaysKele2wise <kele2wise@gmail.com>
2024-04-13 06:38:54 +0100

    i added another formula

--- index.js
+++ index.js
@@ -6,10 +6,16 @@
 
 // dev()
 
-const dev = (par1,par2) => {
+/*const dev = (par1,par2) => {
 console.log({sum:par1+par2})
 console.log("Kelechi ukpai")
 
 }
 
-dev(2, 3)
+dev(2, 3)*/
+
+let x = 5
+let y = 6
+let z = 7 + 5
+
+console.log(x + y + z)
