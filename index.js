5639aadaa3408f2ff34e4e13772acc97db761750
AlwaysKele2wise <kele2wise@gmail.com>
2024-04-16 14:43:19 +0100

    fahrenheit included

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 
-// function dev() {
-// console.log("dev")
-//}
-
-// dev()
-
-
-/*const dev = (par1,par2) => {
-console.log({sum:par1+par2})
-console.log("Kelechi ukpai")
-}
-
-dev(2, 3)*/
-
- 
-
-const express = require('express');
+/*const express = require('express');
 require("dotenv").config();
 
 const app = express();
@@ -37,9 +21,13 @@ app.get("/student", ( req, res, next) => {
 
 app.listen(port, () =>{
     console.log("server is listening on port:" + port)
-})
+})*/
 
 
+/*function myFunction(b1, b2) {
+    console.log(b1 + b2)
+}
+myFunction(7, 6)
 
 /*let x = 5
 let y = 6
@@ -48,9 +36,26 @@ let z = 7 + 5
 console.log(x + y + z)
 
 
+function myFunction(p1, p2){
+    return p1 * p2
+} 
+
+console.log(myFunction(10, 5))
+myFunction(10, 5)*/
+
+function toCelsius(f) {
+    return (f - 32) * 5 / 9
+}
+
+function toFahrenheit(c) {
+    return c * 9 / 5 + 32
+}
+
+console.log(toCelsius(77))
+console.log(toFahrenheit(100))
 
 // Function to compute the product of q1 and q2
-function myFunction() {
+/*function myFunction() {
    const result = 15 * 10
     // return resulty
 
@@ -65,3 +70,18 @@ const dev = (par1,par2) => {
 }
 
 dev(7, 3)*/
+
+
+// function dev() {
+// console.log("dev")
+//}
+
+// dev()
+
+
+/*const dev = (par1,par2) => {
+console.log({sum:par1+par2})
+console.log("Kelechi ukpai")
+}
+
+dev(2, 3)*/
