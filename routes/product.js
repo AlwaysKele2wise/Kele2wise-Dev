c3291f79bf1a81d24ad062c4e6b68a0381745627
AlwaysKele2wise <kele2wise@gmail.com>
2024-04-24 13:36:39 +0100

    Recent changes

--- routes/product.js
+++ routes/product.js
@@ -5,26 +5,18 @@ const router = express.Router()
 
 
 router.post("/", ( req, res, next) => {
-  
-     
            res.send("product created successfully")
-           status: true
  });
 
 router.get("/", ( req, res, next) => {
-    
     res.send({
         product:{
-            name_of_product:"IPAD",
-            model:"IPAD 23434mL",
+            name_of_product: "IPAD",
+            model: "IPAD 23434mL",
             image: "htttots/image",
-            address:"no 1, makanjuola street"
+            address: "no 1, makanjuola street"
         }
     })
 });
 
-router.get("/", ( req, res, next) => {
-
-    res.send("users dashboard")
-});
 module.experts = router
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
-const { Router } = require("express")
+const express = require("express")
 
 const router = express.Router()
 
-//const express = require
 
 router.post("/register", ( req, res, next) => {
-       res.status(201).json({msg:"account created succesfully"})
- });
-router.get("/login", ( req, res, next) => {
+       res.send("account created successfully")
+});
+
+router.post("/login", ( req, res, next) => {
   
      res.send("welcome Kc")
 });
@@ -17,4 +17,4 @@ router.get("/", ( req, res, next) => {
        res.send("users dashboard")
 });
 
-module.experts = router
+module.experts = router 
